bc6964bf4f4c8c69f8c71b41bd7d5022d6e5ea80
js0303 <wlstjr0303@gmail.com>
2023-02-07 17:33:14 +0900

    아직 멀었다..

--- boj_1920.py
+++ boj_1920.py
@@ -1,5 +1,15 @@
-N, A = int(input()), {i: 1 for i in map(int, input().split())}
-M = input()
+# N, A = int(input()), {i: 1 for i in map(int, input().split())}
+# M = input()
 
-for i in list(map(int, input().split())):
-	print(1 if i in A else 0)
+# for i in list(map(int, input().split())):
+# 	print(1 if i in A else 0)
+
+n = int(input())
+arr = set(map(int, input().split()))
+m = int(input())
+x = list(map(int, input().split()))
+for i in x:
+    if i in arr:
+        print(1)
+    else:
+        print(0)
--- boj_2920.py
+++ boj_2920.py
@@ -1,17 +1,27 @@
-arr = list(map(int, input().split(' ')))
+# arr = list(map(int, input().split(' ')))
 
-ascending = True
-descending = True
+# ascending = True
+# descending = True
 
-for i in range(1, 8):
-	if arr[i] > arr[i-1]:
-		descending = False
-	elif arr[i] < arr[i-1]:
-		ascending = False
+# for i in range(1, 8):
+# 	if arr[i] > arr[i-1]:
+# 		descending = False
+# 	elif arr[i] < arr[i-1]:
+# 		ascending = False
 
-if ascending:
+# if ascending:
+#     print("ascending")
+# elif descending:
+#     print("descending")
+# else:
+#     print("mixed")
+
+# 스스로 푼 코드
+lst = list(map(int, input().split()))
+
+if sorted(lst) == lst:
     print("ascending")
-elif descending:
+elif sorted(lst) == lst[::-1]:
     print("descending")
 else:
-    print("mixed")
+	print("mixed")
